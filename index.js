1c023d3871e5ce6dec1d7b637dafbbd592d261e6
francisco191 <silfrancisco99@gmail.com>
2020-11-19 14:48:11 -0300

    se crea archivo carrito.html en views

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3030, () => console.log('Corriendo en el servidor 3030'));
+app.listen(3030, () => {
+    console.log('Corriendo en el servidor 3030');
+})
 
 app.get('/detalle', function(req, res){
     res.sendFile(__dirname + '/src/views/detalle-producto.html');
