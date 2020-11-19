c52ee6faff5bf75d4808a8e1e6e187fe019e5b77
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2020-11-19 09:45:22 -0300

    Esqueleto y CSS del Detalle del Producto casi completo, se agrego un borrador html y css

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get('/detalle', function(req, res){
     res.sendFile(__dirname + '/src/views/detalle-producto.html');
 });
 
+app.get('/borrador', function(req, res){
+    res.sendFile(__dirname + '/src/views/borrador-rodri.html');
+});
+
+app.get('/formulario', function(req, res){
+    res.sendFile(__dirname + '/src/views/formulario-registro.html');
+});
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/' + req.url);
   });
