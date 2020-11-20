019d264e4cae45e231bc58bbcefa66439275a7b9
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2020-11-20 20:38:21 -0300

    Se corrigio el npm start y la ubicacion del app.js

--- src/app.js
+++ src/app.js
@@ -4,17 +4,17 @@ const app = express();
 app.listen(3030, () => console.log('Corriendo en el servidor 3030'));
 
 app.get('/detalle', function(req, res){
-    res.sendFile(__dirname + '/src/views/detalle-producto.html');
+    res.sendFile(__dirname + '/views/detalle-producto.html');
 });
 
 app.get('/borrador', function(req, res){
-    res.sendFile(__dirname + '/src/views/borrador.html');
+    res.sendFile(__dirname + '/views/borrador.html');
 });
 
 app.get('/formulario', function(req, res){
-    res.sendFile(__dirname + '/src/views/formulario-registro.html');
+    res.sendFile(__dirname + '/views/formulario-registro.html');
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/' + req.url);
+    res.sendFile(__dirname.replace("\src", "") + '/public/' + req.url);
   });
