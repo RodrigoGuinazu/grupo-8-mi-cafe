a0254c79d54c244444726a573a1425ced700ab98
analiavargas <vargas_va@live.com.ar>
2021-01-06 13:39:27 -0300

    Se definieron las rutas del Home

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var indexController = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/conoceme', indexController.nosotrosMC);
+router.get('/viaja', indexController.regionesMC);
+router.get('/encontralo', indexController.perfilMC);
 
 module.exports = router;
