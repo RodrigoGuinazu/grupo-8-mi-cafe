208ae8c138448ba81009d4ec45fb1993fdbc9dd0
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-01-13 10:20:30 -0300

    Recomendaciones generada al azar y forEach en la vista del carrito

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -1,6 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+let products = fs.readFileSync(path.resolve(__dirname, '../data/productos.json'), {encoding: 'utf-8'});
+products = JSON.parse(products);
+
 let carritoController = {
     carritoLleno: function(req, res) {
-        res.render('products/carrito-lleno');
+        let recomendacion = []
+
+        let randomNum = function() {
+            const numeroRandom = products[Math.floor(Math.random() * products.length)];
+            if (recomendacion.includes(numeroRandom)) {
+                randomNum()
+            } else {
+                recomendacion.push(numeroRandom);
+            }
+        }
+
+        while(recomendacion.length < 4){
+            randomNum()
+        }
+
+        res.render('products/carrito-lleno', {recomendacion});
     },
     carritoVacio: function(req, res) {
         res.render('products/carrito-vacio');
