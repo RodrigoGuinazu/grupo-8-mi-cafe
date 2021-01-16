077fe5cbd322502fd37b98caba4af430e60e4fde
francisco191 <silfrancisco99@gmail.com>
2021-01-16 12:40:47 -0300

    se intentan corregir errores de ruta al intentar loguearse

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const { check, validationResult, body } = require("express-validator");
 
-let users = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {encoding: 'utf-8'});
-users = JSON.parse(users);
+//let users = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {encoding: 'utf-8'});
+//users = JSON.parse(users);
 
 let loginController = {
     login: function(req, res) {
@@ -20,10 +20,33 @@ let loginController = {
         let errors = validationResult(req);
 
         if(errors.isEmpty) {
+            let usersJSON = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {encoding: 'utf-8'});
+            let usuarios;
+            if(usersJSON == "") {
+                usuarios = [];
+            } else {
+                usuarios = JSON.parse(usersJSON);
+            }
+
+            for(let i = 0; i < usuarios.length; i++) {
+                if(req.body.email == usuarios[i].email) {
+                    if(bcrypt.compareSync(req.body.password, usuarios[i].password)) {
+                        let usuarioALoguearse = usuarios[i];
+                        break;
+                    }
+                } 
+            }
+
+            if (usuarioALoguearse == undefined) { // => Si el usuario aun es undefined que arroje errores. Si ya encontro y valido email y contraseña en el for, ¿Por que seria undefined?
+                return res.render("login", { errors: [
+                    {msge: "Credenciales Invalidas"}
+                ]})
+            }
 
         } else {
-            return res.render("login", {errors: errors.errors});
+            return res.render("login", {errors: errors.errors}); // => consultar la logica de errors: errors.errors
         }
     }
+}
 
 module.exports = loginController;
--- src/routes/login.js
+++ src/routes/login.js
@@ -5,7 +5,7 @@ const { check, validationResult, body } = require("express-validator");
 let router = express.Router();
 router.get ('', loginController.login)
 router.post('', [
-    check("email").isEmail().withMessage("El email no es válido")
+    check("email").isEmail().withMessage("El email no es válido"),
     check("password").isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres")
 ] ,loginController.processLogin)
 //router.get ('/wrong-user', loginController.wrongUser);
