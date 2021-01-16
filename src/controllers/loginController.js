6ca82a9217b2e42f12655c9206b57967c5dec9e7
francisco191 <silfrancisco99@gmail.com>
2021-01-16 11:58:47 -0300

    se continua la logica del processLogin y sus validaciones

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,36 +1,29 @@
 // const bcrypt = require("bcrypt"); => DEBE REQUERIRSE EN EL REGISTRO
 const fs = require('fs');
 const path = require('path');
+const { check, validationResult, body } = require("express-validator");
 
 let users = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {encoding: 'utf-8'});
 users = JSON.parse(users);
 
 let loginController = {
     login: function(req, res) {
-        res.render('users/login');           
+        return res.render('users/login');           
     },
-    wrongUser: function(req, res) {
-        res.render('users/login-wrong-user');
-    },
-    userNotFound: function(req, res) {
-        res.render('users/login-user-not-found');
-    },
+    //wrongUser: function(req, res) {
+        //res.render('users/login-wrong-user');
+    //},
+    //userNotFound: function(req, res) {
+        //res.render('users/login-user-not-found');
+    //},
     processLogin: function(req, res) {
-        //let usuarios;
-        //if(users == "") {
-            //usuarios = []
-        //} else {
-            //usuarios = JSON.parse(users)
-        //}
+        let errors = validationResult(req);
 
-        for(let i = 0; i < users.length; i++) {
-            if(req.body.email == users[i].email && req.body.password == users[i].password) {
-                res.send("Estas logueado")
-            } else {
-                res.send("Error")
-            }
+        if(errors.isEmpty) {
+
+        } else {
+            return res.render("login", {errors: errors.errors});
         }
     }
-}
 
 module.exports = loginController;
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,14 @@
 let express = require('express');
 let loginController = require('../controllers/loginController');
+const { check, validationResult, body } = require("express-validator");
 
 let router = express.Router();
 router.get ('', loginController.login)
-router.post('', loginController.processLogin)
-router.get ('/wrong-user', loginController.wrongUser);
-router.get ('/user-not-found', loginController.userNotFound);
+router.post('', [
+    check("email").isEmail().withMessage("El email no es válido")
+    check("password").isLength({ min: 8 }).withMessage("La contraseña debe tener al menos 8 caracteres")
+] ,loginController.processLogin)
+//router.get ('/wrong-user', loginController.wrongUser);
+//router.get ('/user-not-found', loginController.userNotFound);
 
 module.exports = router;
