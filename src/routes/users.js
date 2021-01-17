ec79fb4bb40cc831a2fccf71d5919db25037a75a
analiavargas <vargas_va@live.com.ar>
2021-01-17 00:01:21 -0300

    Se incorporaron y setearon las rutas del Register

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+let { check, validationResult, body } = require('express-validator');
+let registerController = require('../controllers/registerController');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get ('/register', registerController.register);
+router.post('/register', [
+	check('email').isEmail().withMessage('Email inválido'),
+  check('password').isLength( {min: 8} ),
+  body('confirmar-password').custom(function(value) {
+    if (req.body.password != req.body.confirmar-password) {
+      return false;
+    } else {
+      return true;
+    }
+  }).withMessage('Las contraseñas deben coincidir.')
+], registerController.processRegister);
 
 module.exports = router;
