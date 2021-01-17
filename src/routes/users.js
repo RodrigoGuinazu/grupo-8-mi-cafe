4c2ab54521c48f5145f33d9fddb80ae09af84cdf
analiavargas <vargas_va@live.com.ar>
2021-01-17 16:42:21 -0300

    Seteo de rutas register

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,22 +4,26 @@ let { check, validationResult, body } = require('express-validator');
 let registerController = require('../controllers/registerController');
 
 
-/* GET users listing. */
+/* GET users listing.
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+*/
 
 router.get ('/register', registerController.register);
-router.post('/register', [
-	check('email').isEmail().withMessage('Email inválido'),
-  check('password').isLength( {min: 8} ),
-  body('confirmar-password').custom(function(value) {
+router.post('/login', /*[
+  check('nombre').isLength().withMessage('Ingresá tu nombre.'),
+  check('apellido').isLength().withMessage('Ingresá tu apellido.'),
+	check('email').isEmail().withMessage('Ingresá un Email válido.'),
+  check('password').isLength( {min: 8} ).withMessage('Las contraseñas tener al menos 8 caracteres.'),
+  check('confirmar-password').equals('password').withMessage('Las contraseñas deben coincidir.')
+  /*body('confirmar-password').custom(function(value) {
     if (req.body.password != req.body.confirmar-password) {
       return false;
     } else {
       return true;
     }
   }).withMessage('Las contraseñas deben coincidir.')
-], registerController.processRegister);
+],*/ registerController.processRegister);
 
 module.exports = router;
