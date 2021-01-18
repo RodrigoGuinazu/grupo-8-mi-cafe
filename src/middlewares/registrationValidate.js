655f3cfdff89cbd1590eeb6570fbdf2a309d9c86
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-01-17 21:36:18 -0300

    Se creo el middleware registrationValidate.js

---
+++ src/middlewares/registrationValidate.js
@@ -0,0 +1,14 @@
+const {check} = require('express-validator');
+module.exports = [
+    check('nombre').isLength().withMessage('Ingresá tu nombre.'),
+    check('apellido').isLength().withMessage('Ingresá tu apellido.'),
+      check('email').isEmail().withMessage('Ingresá un Email válido.'),
+    check('password').isLength( {min: 8} ).withMessage('La contraseña debe tener al menos 8 caracteres.'),
+    /*body('confirmarPassword').custom(function(value) {
+      if ('password' != 'confirmarPassword') {
+        return true;
+      } else {
+        return false;
+      }
+    }).withMessage('Las contraseñas deben coincidir.')*/
+  ]
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,24 +4,14 @@ let multer = require('multer');
 let multerUsuarios = require('../middlewares/multerUsuarios');
 let { check, validationResult, body } = require('express-validator');
 let usersController = require('../controllers/usersController');
+const registrationValidate = require('../middlewares/registrationValidate')
 
 
 var router = express.Router();
 router.get ('/login', usersController.login);
 router.post('/login',usersController.processLogin);
 router.get ('/register', usersController.register);
-router.post('/register', multerUsuarios.any(), [
-  check('nombre').isLength().withMessage('Ingresá tu nombre.'),
-  check('apellido').isLength().withMessage('Ingresá tu apellido.'),
-	check('email').isEmail().withMessage('Ingresá un Email válido.'),
-  check('password').isLength( {min: 8} ).withMessage('La contraseña debe tener al menos 8 caracteres.'),
-  /*body('confirmarPassword').custom(function(value) {
-    if ('password' != 'confirmarPassword') {
-      return true;
-    } else {
-      return false;
-    }
-  }).withMessage('Las contraseñas deben coincidir.')*/
-], usersController.processRegister);
+router.post('/register', multerUsuarios.any(), registrationValidate, usersController.processRegister);
+router.get ('/editar', usersController.editar);
 
 module.exports = router;
