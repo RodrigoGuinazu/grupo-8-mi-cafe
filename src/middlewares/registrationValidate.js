1cf7e13d555bcf13ee7d35a7c2a1213aff3a3a84
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-01-19 19:06:08 -0300

    Se arreglo el registro de usuario

--- src/middlewares/registrationValidate.js
+++ src/middlewares/registrationValidate.js
@@ -1,8 +1,9 @@
 const {check} = require('express-validator');
+
 module.exports = [
     check('nombre').isLength().withMessage('Ingresá tu nombre.'),
     check('apellido').isLength().withMessage('Ingresá tu apellido.'),
-    check('email').isEmail().withMessage('Ingresá un Email válido.'),
+    check('email').isEmail().withMessage('Ingresá un E-mail válido.'),
     check('password').isLength( {min: 8} ).withMessage('La contraseña debe tener al menos 8 caracteres.'),
     /*body('confirmarPassword').custom(function(value) {
       if ('password' != 'confirmarPassword') {
