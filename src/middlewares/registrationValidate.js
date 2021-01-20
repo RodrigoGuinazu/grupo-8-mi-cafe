d5d1a5951a4daff7a28706f39ad53b7f8f48efc1
analiavargas <vargas_va@live.com.ar>
2021-01-19 22:12:52 -0300

    Se incluyó check en nombre y apellido

--- src/middlewares/registrationValidate.js
+++ src/middlewares/registrationValidate.js
@@ -1,8 +1,8 @@
 const {check} = require('express-validator');
 
 module.exports = [
-    check('nombre').isLength().withMessage('Ingresá tu nombre.'),
-    check('apellido').isLength().withMessage('Ingresá tu apellido.'),
+    check('nombre').isLength({min:1}).withMessage('Ingresá tu nombre.'),
+    check('apellido').isLength({min:1}).withMessage('Ingresá tu apellido.'),
     check('email').isEmail().withMessage('Ingresá un E-mail válido.'),
     check('password').isLength( {min: 8} ).withMessage('La contraseña debe tener al menos 8 caracteres.'),
     /*body('confirmarPassword').custom(function(value) {
