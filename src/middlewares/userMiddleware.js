0848e0e8593be7523a50883ef626c9861851358f
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-01-20 12:58:23 -0300

    Solo el admin puede entrar a /crear y /editar

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -14,7 +14,7 @@ let userMiddleware = {
         }
     },
     admin: function(req, res, next){
-        if(req.session.usuarioALoguearse && locals.user.role == 'admin'){
+        if(req.session.usuarioALoguearse && req.session.usuarioALoguearse.role == 'admin'){
             next();
         } else {
             res.redirect('/')
