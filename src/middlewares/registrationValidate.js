b049e84ccc3fe3ddb2de13922ded04cfbe102905
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-02 03:22:46 -0300

    Se implemento el metodo isStrong para la password

--- src/middlewares/registrationValidate.js
+++ src/middlewares/registrationValidate.js
@@ -1,15 +1,16 @@
-const {check} = require('express-validator');
+const {check, body} = require('express-validator');
 
 module.exports = [
     check('nombre').isLength({min:1}).withMessage('Ingresá tu nombre.'),
     check('apellido').isLength({min:1}).withMessage('Ingresá tu apellido.'),
     check('email').isEmail().withMessage('Ingresá un E-mail válido.'),
-    check('password').isLength( {min: 8} ).withMessage('La contraseña debe tener al menos 8 caracteres.'),
-    /*body('confirmarPassword').custom(function(value) {
-      if ('password' != 'confirmarPassword') {
-        return true;
-      } else {
-        return false;
-      }
-    }).withMessage('Las contraseñas deben coincidir.')*/
+    check('password').isStrongPassword().withMessage('La contraseña debe tener al menos 8 caracteres, 1 Mayuscula, 1 Numero y 1 Caracter especial.'),
+    body('confirmarPassword')
+        .custom(function(value, {req}) {
+            if (value == req.body.password) {
+                return true;
+            }
+            return false;
+        })
+        .withMessage('Las contraseñas no coinciden, intentá nuevamente')
   ]
