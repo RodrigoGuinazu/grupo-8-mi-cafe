5ed35cd69f4e7a221189fb55915eccf81496352d
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-02 03:33:30 -0300

    Se creo logica para evitar que se registre un email 2 veces

--- src/middlewares/registrationValidate.js
+++ src/middlewares/registrationValidate.js
@@ -1,4 +1,9 @@
 const {check, body} = require('express-validator');
+const path = require('path');
+const fs = require('fs');
+
+let users = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {encoding: 'utf-8'});
+users = JSON.parse(users);
 
 module.exports = [
     check('nombre').isLength({min:1}).withMessage('Ingresá tu nombre.'),
@@ -12,5 +17,21 @@ module.exports = [
             }
             return false;
         })
-        .withMessage('Las contraseñas no coinciden, intentá nuevamente')
+        .withMessage('Las contraseñas no coinciden, intentá nuevamente'),
+    body('email')
+    .custom(function(value, {req}) {
+        for (let i = 0; i < users.length; i++) {
+            if(users[i].email == value) {
+                if (req.session.usuarioALoguearse != undefined) {
+                    if (users[i].id == req.session.usuarioALoguearse.id) {
+                        return true;
+                    }
+                    return false;
+                }
+                return false;
+            }
+        }
+        return true;
+    })
+    .withMessage('El email que ingresaste ya está registrado')
   ]
