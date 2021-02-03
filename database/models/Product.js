779ecb391bd22bee7bebb17ff9fa3147c573b522
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-03 17:54:16 -0300

    Se agregaron las relaciones en el modelo Product.js

--- database/models/Product.js
+++ database/models/Product.js
@@ -40,39 +40,21 @@ module.exports = (sequelize, dataTypes) => {
     };
 
     const Product = sequelize.define(alias, cols, config);
+
+    Product.associate = function(models){
+        Product.belongsTo(models.Color, {
+            as: "colors",
+            foreignKey: "colors_id"
+        }),
+        Product.belongsTo(models.Weight, {
+            as: "weights",
+            foreignKey: "weight_id"
+        }),
+        Product.belongsTo(models.Category, {
+            as: "categories",
+            foreignKey: "category_id"
+        })
+    }
     
     return Product
 }
-
-//CREATE TABLE IF NOT EXISTS `mi_cafe`.`products` (
-    //`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    //`name` VARCHAR(100) NOT NULL,
-    //`price` DECIMAL NOT NULL,
-    //`description` TEXT NOT NULL,
-    //`image` VARCHAR(100) NOT NULL,
-    //`category_id` BIGINT UNSIGNED NOT NULL,
-    //`weight_id` BIGINT UNSIGNED NULL,
-    //`colors_id` BIGINT UNSIGNED NULL,
-    //`created_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-    //`updated_at` TIMESTAMP NULL DEFAULT NULL,
-    //`deleted_at` TIMESTAMP NULL,
-    PRIMARY KEY (`id`),
-    INDEX `category_id_idx` (`category_id` ASC),
-    INDEX `weight_id_idx` (`weight_id` ASC),
-    INDEX `colors_id_idx` (`colors_id` ASC),
-    CONSTRAINT `category_id`
-      FOREIGN KEY (`category_id`)
-      REFERENCES `mi_cafe`.`categories` (`id`)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-    CONSTRAINT `weight_id`
-      FOREIGN KEY (`weight_id`)
-      REFERENCES `mi_cafe`.`weights` (`id`)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-    CONSTRAINT `colors_id`
-      FOREIGN KEY (`colors_id`)
-      REFERENCES `mi_cafe`.`colors` (`id`)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION)
-  ENGINE = InnoDB
