892c3d257d4484de684607433e860f1c5369ab7c
francisco191 <silfrancisco99@gmail.com>
2021-02-02 21:44:18 -0300

    se modifica el archivo data.sql

--- database/data.sql
+++ database/data.sql
@@ -5,9 +5,12 @@ insert into users (email, password,role_id,created_at) values ('francisco@gmail.
 insert into users (email, password,role_id,created_at) values ('rodrigosan06@gmail.com', '$2b$10$SX9/59B3nGTPqn0Gwa4Apeo4l97uOGQY3XFLNQKvvDx6VKSAzswdu',2,now());
 insert into users (email, password,role_id,created_at) values ('rodrigo@hotmail.com', '$2b$10$y0PFqCKB3xKZ4nMHsG/wu.nA/6b7BSGtgBNoA7hlUuBLXwHFZu6kW',2,now());
 insert into users (email, password,role_id,created_at) values ('anav@gmail.com', '$2b$10$tDYdKWkvnskPAKdwhrTZpucLk4mQQJUgfQrPCo1sv8M.ke/vKwb2u',2,now());
+-- modificado 02/02 21:30hs FS --
 insert into users (email, password,role_id,created_at) values ('chicos@gmail.com', '$2b$10$91Umf3AjCqjb5fPuGE1.Su5DgJgE8KtBqdfCSPrkQ060m3ARhjMQu',2,now());
 
 -- Tabla roles
 
 insert into roles (type,created_at) values ('admin',now());
 insert into roles (type,created_at) values ('user',now());
+-- modificado 02/02 21:30hs FS --
+insert into users (email, password,role_id,created_at) values ('chicos@gmail.com', '$2b$10$91Umf3AjCqjb5fPuGE1.Su5DgJgE8KtBqdfCSPrkQ060m3ARhjMQu',2,now());
