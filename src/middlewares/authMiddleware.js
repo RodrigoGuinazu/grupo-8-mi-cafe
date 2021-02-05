5d508aeabce5fbd27216ee826300def14fe5695d
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-05 17:20:49 -0300

    Se empezo a corregir la logica de las cookies

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,7 +2,7 @@ module.exports = function(req, res, next) {
 
     if(req.session.usuarioALoguearse){
         res.locals.user = req.session.usuarioALoguearse;
-    } else if(req.cookies.usuarioALoguearse){
+    } else if(req.cookies.usuario){
         req.session.usuarioALoguearse = req.cookies.usuario;
         res.locals.user = req.cookies.usuario;
     }
