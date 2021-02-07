3ba8b25d292cbe617a21c1bd2bb277634ab17090
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-07 04:20:02 -0300

    Intentando modificar la logica del Login para que funcione con la DB

--- database/config/config.js
+++ database/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     "port": process.env.PORT_DB,
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
+    "operatorsAliases": 0
   },
   "test": {
     "username": "root",
@@ -16,7 +16,7 @@ module.exports = {
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
+    "operatorsAliases": 0
   },
   "production": {
     "username": "root",
@@ -24,6 +24,6 @@ module.exports = {
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "operatorsAliases": false
+    "operatorsAliases": 0
   }
 }
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -5,65 +5,59 @@ const bcrypt = require('bcrypt');
 const db = require('../../database/models')
 const sequelize = require('sequelize')
 
-
-let users = fs.readFileSync(path.resolve(__dirname, '../data/usuarios.json'), {encoding: 'utf-8'});
-users = JSON.parse(users);
-
 let usersController = {
+    // VISTA LOGIN
     login: function(req, res) {
         return res.render('users/login');           
     },
-    //wrongUser: function(req, res) {
-        //res.render('users/login-wrong-user');
-    //},
-    //userNotFound: function(req, res) {
-        //res.render('users/login-user-not-found');
-    //},
+    // LOGICA LOGIN
     processLogin: function(req, res) {
         let errors = validationResult(req);
 
-        let usuarioALoguearse = db.User.findOne({where: { email: req.body.email }})
-            .then(function(usuarioALoguearse){
-                if(usuarioALoguearse != undefined){
-                    if(bcrypt.compare(req.body.password, usuarioALoguearse.password)){
-                        req.session.usuarioALoguearse = usuarioALoguearse;
+        db.User.findOne({
+            where: {
+                email: req.body.email
+            }
+        } 
+        )
+        .then(usuarioALoguearse => {
+            if(usuarioALoguearse){
+                if(bcrypt.compare(req.body.password, usuarioALoguearse.password)){
+                    req.session.usuarioALoguearse = usuarioALoguearse;
 
-                        if(req.body.remindme){
-                            res.cookie('usuario', usuarioALoguearse.email, {maxAge: 1000 * 60 * 60})
-                            res.locals.usuarioALoguearse = req.session.usuarioALoguearse
-                        }
-
-                        res.redirect('/');
-                    } else{
-                        res.render('usres/login', {errors: [
-                            {msg: "Credenciales incorrectas"}
-                        ]})
+                    if(req.body.remindme){
+                        res.cookie('usuario', req.session.usuarioALoguearse, {maxAge: 1000 * 60 * 60})
+                        res.locals.usuarioALoguearse = req.session.usuarioALoguearse
                     }
-                    } else{
-                        res.render("users/login", { errors: [
-                            {msg: "No existe ningun usuario registrado con ese email"}
-                        ]})
-                    }
+                    res.redirect('/');
+                } else{
+                    res.render('users/login',
+                    {errors: [{msg: "Credenciales incorrectas"}]})
+                }
+                } else{
+                    res.render("users/login",
+                    { errors: [{msg: "Credenciales incorrectas"}]})
+                }             
         })
         .catch(function(error){
             console.log(error);
         })
     },
+    // LOGICA LOGOUT
     logout: function(req, res){
         req.session.destroy();
         res.redirect('/usuarios/login')
     },
+    // VISTA REGISTRO
     register: function(req, res) {
         res.render('users/formulario-registro');
     },
+    // LOGICA DE REGISTRO
     processRegister: function(req, res, next) {
-
         const errors = validationResult(req);
         if(!errors.isEmpty()){
             return res.render('users/formulario-registro', {errors: errors.errors});
         } else {
-        /*    let arrayId = [];*/
-
             db.User.create({
                 email: req.body.email,
                 password: bcrypt.hashSync(req.body.password, 10),
@@ -71,73 +65,32 @@ let usersController = {
             })
         }
         res.redirect("/usuarios/login");
-        
-        /*arrayId = users.map(function(obj) {
-            return obj.id
-        })
-
-        let mayorId = arrayId.reduce((a, b) => {
-            if(a > b) {
-                return a
-            } else {
-                return b
-            }
-        })
-        let nuevoId = mayorId+1;
-
-        let user = {
-            id: nuevoId,
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, 10),
-            fechaNacimiento: "",
-            direccion: "",
-            genero: "",
-            imagen: "",
-            role: "user",
-        }
-
-        users.push(user);
-        let usersJSON = JSON.stringify(users);
-        fs.writeFileSync(path.join(__dirname, '../data/usuarios.json'), usersJSON);
-        res.redirect("/usuarios/login");
-        }
-        */
-        
     },
+    // VISTA EDITAR
     editar: function(req, res) {
         res.render('users/editar-usuario');
     },
+    // LOGICA EDICION
     modificacion: (req, res) => {
-        if(req.files[0] != undefined){
-            db.Profile.update({
-                name: req.body.nombre,
-                last_name: req.body.apellido,
-                birthdate: req.body.fechaNacimiento,
-                gender: req.body.genero,
-                image: req.files[0].filename
-            },
-            {
-                where: {
-                    usuarioALoguearse.id == User.id
-                }
+        db.Profile.update({
+            name: req.body.nombre,
+            last_name: req.body.apellido,
+            birthdate: req.body.fechaNacimiento,
+            gender: req.body.genero
+        },
+        {
+            where: {
+                id: res.locals.user.id
+            }
+        })
+        .then( resultado => {
+            res.redirect('/usuarios/login')
             })
-        } else{
-            db.Profile.update({
-                name: req.body.nombre,
-                last_name: req.body.apellido,
-                birthdate: req.body.fechaNacimiento,
-                gender: req.body.genero,
-                image: image
-            },
-            {
-                where: {
-                    usuarioALoguearse.id == user.id
-                }
-            })
-        }
+        .catch(error => {
+            res.send(error)
+        })
     },
+    // VISTA PERFIL
     perfil: function(req, res) {
         res.render('users/perfil-usuario');
     }
