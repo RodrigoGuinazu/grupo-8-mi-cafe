3c003e8f34ce536da73e6206b79b108a520c12b7
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-02-18 12:46:21 -0300

    Cambio menor

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-//let products = fs.readFileSync(path.resolve(__dirname, '../data/productos.json'), {encoding: 'utf-8'});
-//products = JSON.parse(products);
+let products = fs.readFileSync(path.resolve(__dirname, '../data/productos.json'), {encoding: 'utf-8'});
+products = JSON.parse(products);
 
 let carritoController = {
     carritoLleno: function(req, res) {
