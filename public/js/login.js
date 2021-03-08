2eb8c4baa74fc146ee5dc770256894fb98926702
francisco <silfrancisco99@gmail.com>
2021-03-08 14:01:26 -0300

    se hacen correcciones sobre las validaciones front del login

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,29 @@
 window.addEventListener("load", function() {
-    let formulario = document.querySelector("form.login-credential");
 
+    /*let formulario = document.querySelector("form.login-credential");
     formulario.addEventListener("submit", function(e) {
-        e.preventDefault();
+        e.preventDefault();*/
 
-        let errores = [];
+        const campoEmail = document.querySelector("input.email");
+        const campoPassword = document.querySelector("input.password");
+        const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+        const emailError = document.querySelector(".erroresEmail")
+        const passwordError = document.querySelector(".erroresPassword")
 
-        let campoEmail = document.querySelector("input.email");
-
-        let validarEmail = function(campoEmail.value) {
+       /* let validarEmail = function(campoEmail.value) {
             if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(campoEmail.value)){
                 alert("La dirección de email " + campoEmail.value + " es correcta.");
                } else {
                 errores.push("La dirección de email es incorrecta.");
                }
-        }
-        
-        if (campoEmail.value == "") {
-            errores.push("El campo de E-mail debe estar completo.")
-        }
-
-        let campoPassword = document.querySelector("input.password");
+        } */
+        campoEmail.addEventListener ("onblur", function () {
+            if (campoEmail.value == "") {
+                emailError.innerHTML("El campo de E-mail debe estar completo.")
+            } else (regexEmail.test(campoEmail.value) == false) {
+                emailError.innerHTML("El campo debe tener formato de E-mail.")
+            }
+        })
 
 
     })
-})
