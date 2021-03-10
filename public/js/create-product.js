e481fdfd00f8eb9af8e3a4dae5fc5a3e6d1e4a65
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-03-09 23:36:11 -0300

    Trabajando la validacion desde el front para la creacion de productos

--- public/js/create-product.js
+++ public/js/create-product.js
@@ -1,59 +1,62 @@
-window.addEventListener('load', function(){
-    let form = document.querySelector('form')
-    form.addEventListener('blur', function(){
-        /*let errores = {
-            image: "",
-            name: "",
-            price: "",
-            description: ""
-        }*/
+let formCreate = document.querySelector('form.form-create')
+console.log(formCreate)
+formCreate.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    let errores = {
+        name: "",
+        price: "",
+        image: "",
+        description: ""
+    }
+
+    /*let erroresName = document.querySelector(".erroresName");
+    let erroresPrice = document.querySelector(".erroresPrice");
+    let erroresImg = document.querySelector(".erroresImg");
+    let erroresDescription = document.querySelector(".erroresDescription");*/
+
+    let name = document.querySelector('input.name');
+    if(name.value == ""){
+        errores.name == "Debes ponerle un nombre al articulo"
+        //erroresName.innerHTML = "Debes ponerle un nombre al articulo"
+    }else if(name.value.length < 5){
+        errores.name == "El titulo debe tener al menos 5 caracteres"
+        //erroresName.innerHTML = "El titulo debe tener al menos 5 caracteres"
+    }
+
+    let price = document.querySelector('input.price');
+    if(price.value < 0){
+        erroresPrice.innerHTML = "El precio deber ser mayor a $0"
+    }
+
+    let image = document.querySelector('input.product-img');
+    if(image.value == ""){
+        erroresImg.innerHTML = "Debes subir la imagen del Producto"
+    }
+
+    let description = document.querySelector('textarea.description');
+    if(description.value == ""){
+        erroresDescription.innerHTML = "La descripcion no puede estar vacia"
+    }else if(description.value.length < 20){
+        erroresDescription.innerHTML = "La descripcion debe tener al menos 20 caracteres"
+    }
+
+    if(errores.image != "" || errores.name != "" || errores.price != "" || errores.description != ""){
 
         let erroresImg = document.querySelector(".erroresImg");
+    
+        erroresImg.innerHTML = errores.image;
+        
         let erroresName = document.querySelector(".erroresName");
+        
+        erroresName.innerHTML = errores.name;
+
         let erroresPrice = document.querySelector(".erroresPrice");
+        
+        erroresPrice.innerHTML = errores.price
+
         let erroresDescription = document.querySelector(".erroresDescription");
-
-        let image = document.querySelector('input.product-img');
-        if(image.value == ""){
-            erroresImg.innerHTML = "Debes subir la imagen del Producto"
-        }
-
-        let name = document.querySelector('input.name');
-        if(name.value == ""){
-            erroresName.innerHTML = "Debes ponerle un nombre al articulo"
-        }else if(name.value.length < 5){
-            erroresName.innerHTML = "El titulo debe tener al menos 5 caracteres"
-        }
-
-        let price = document.querySelector('input.price');
-        if(price.value < 0){
-            erroresPrice.innerHTML = "El precio deber ser mayor a $0"
-        }
-
-        let description = document.querySelector('textarea.description');
-        if(description.value == ""){
-            erroresDescription.innerHTML = "La descripcion no puede estar vacia"
-        }else if(description.value.length < 20){
-            erroresDescription.innerHTML = "La descripcion debe tener al menos 20 caracteres"
-        }
-
-        /*if(errores.image != "" || errores.name != "" || errores.price != "" || errores.description != ""){
-            let erroresImg = document.querySelector(".erroresImg");
-       
-            erroresImg.innerHTML = errores.image;
-            
-            let erroresName = document.querySelector(".erroresName");
-            
-            erroresName.innerHTML = errores.name;
-
-            let erroresPrice = document.querySelector(".erroresPrice");
-            
-            erroresPrice.innerHTML = errores.price
-
-            let erroresDescription = document.querySelector(".erroresDescription");
-            
-            erroresDescription.innerHTML = errores.description
-        }*/
-    })
-
+        
+        erroresDescription.innerHTML = errores.description
+    }
 })
