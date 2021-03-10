aa6e9074313ea71716d227f2279c492ebc391134
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-03-10 17:12:47 -0300

    Se agregaron comentarios en el archivo create-product.js

--- public/js/create-product.js
+++ public/js/create-product.js
@@ -8,50 +8,49 @@ window.addEventListener('load', function() {
     // ERRORES CON BLUR //
     
     let name = document.querySelector('input.name');
-    name.addEventListener('blur', function(){
+    name.addEventListener('blur', function(){ // Cuando el admin sale del input sin que se cumplan las condiciones
         if(name.value == ""){
             erroresName.innerHTML = "Debes ponerle un nombre al articulo"
         }else if(name.value.length < 5){
             erroresName.innerHTML = "El titulo debe tener al menos 5 caracteres"
         }else {
-            erroresName.innerHTML = ""
+            erroresName.innerHTML = "" // En caso de que se cumplan las condiciones no mostramos ningun error
         }
     })
 
     let price = document.querySelector('input.price');
-    price.addEventListener('blur', function(){
+    price.addEventListener('blur', function(){ // Cuando el admin sale del input sin que se cumplan las condiciones
         if(price.value <= 0){
             erroresPrice.innerHTML = "El precio deber ser mayor a $0"
         }else {
-            erroresPrice.innerHTML = ""
+            erroresPrice.innerHTML = "" // En caso de que se cumplan las condiciones no mostramos ningun error
         }
     })
 
     let image = document.querySelector('input.product-img');
-    image.addEventListener('blur', function(){
+    image.addEventListener('blur', function(){ // Cuando el admin sale del input sin que se cumplan las condiciones
         if(image.value == ""){
             erroresImg.innerHTML = "Debes subir la imagen del Producto"
         }else {
-            erroresImg.innerHTML = ""
+            erroresImg.innerHTML = "" // En caso de que se cumplan las condiciones no mostramos ningun error
         }
     })
 
     let description = document.querySelector('textarea.description');
-    description.addEventListener('blur', function(){
+    description.addEventListener('blur', function(){ // Cuando el admin sale del input sin que se cumplan las condiciones
         if(description.value == ""){
             erroresDescription.innerHTML = "La descripcion no puede estar vacia"
         }else if(description.value.length < 20){
             erroresDescription.innerHTML = "La descripcion debe tener al menos 20 caracteres"
         }else {
-            erroresDescription.innerHTML = ""
+            erroresDescription.innerHTML = "" // En caso de que se cumplan las condiciones no mostramos ningun error
         }
     })
 
     // ERRORES CUANDO EL ADMIN APRETA SUBMIT //
 
     let formCreate = document.querySelector('form.form-create')
-    formCreate.addEventListener('submit', function(e){
-        e.preventDefault();
+    formCreate.addEventListener('submit', function(e){ // Todo este codigo funciona cuando el Admin apreta el boton submit
 
         let errores = {
             name: "",
@@ -85,7 +84,7 @@ window.addEventListener('load', function() {
             errores.description = "La descripcion debe tener al menos 20 caracteres"
         }
 
-        if(errores.image != "" || errores.name != "" || errores.price != "" || errores.description != ""){
+        if(errores.image != "" || errores.name != "" || errores.price != "" || errores.description != ""){ // En caso de que haya un error hace lo siguiente
             e.preventDefault();
 
             erroresName.innerHTML = "Debes ponerle un nombre al articulo"
@@ -98,7 +97,7 @@ window.addEventListener('load', function() {
             erroresDescription.innerHTML = "La descripcion no puede estar vacia"
             erroresDescription.innerHTML = "La descripcion debe tener al menos 20 caracteres"
         }else {
-            formCreate.submit()
+            formCreate.submit() // En el caso de que la condicion de arriba sea falsa (que no haya ningun error) permitimos el submit de formCreate
         }
     })
 })
