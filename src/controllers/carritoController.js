7d74114dacafd95859d1dd1bcc96d53093741053
RodrigoGuinazu <rodrigoguinazu@outlook.com>
2021-03-20 18:43:24 -0300

    Agregando las recomendaciones via db en el carrito

--- src/controllers/carritoController.js
+++ src/controllers/carritoController.js
@@ -1,27 +1,15 @@
-const fs = require('fs');
-const path = require('path');
-
-let products = fs.readFileSync(path.resolve(__dirname, '../data/productos.json'), {encoding: 'utf-8'});
-products = JSON.parse(products);
+const db = require('../../database/models');
+const Product = require('../../database/models/Product');
 
 let carritoController = {
     carrito: function(req, res) {
-        let recomendacion = []
-
-        let randomNum = function() {
-            const numeroRandom = products[Math.floor(Math.random() * products.length)];
-            if (recomendacion.includes(numeroRandom)) {
-                randomNum()
-            } else {
-                recomendacion.push(numeroRandom);
-            }
-        }
-
-        while(recomendacion.length < 4){
-            randomNum()
-        }
-
-        res.render('products/carrito', {recomendacion});
+        db.Product.findAll({ limit: 4 })
+        .then((recomendacion) => {
+            res.render('products/carrito', {recomendacion});
+        })
+        .catch(function(error){
+            console.log(error);
+        })
     }
 }
 
