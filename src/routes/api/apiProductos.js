874a5b467e5677d0750e66e800db72c1c4b9e66f
francisco <silfrancisco99@gmail.com>
2021-04-06 15:51:14 -0300

    se establecen las rutas de acceso a la api de productos

--- src/routes/api/apiProductos.js
+++ src/routes/api/apiProductos.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const apiProductosController = require('../../controllers/api/apiProductosController');
+
+router.get('/', apiProductosController.listado);
+router.get('/:id', apiProductosController.detalle);
+
+module.exports = router;
