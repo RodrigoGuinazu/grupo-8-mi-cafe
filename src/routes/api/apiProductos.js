abb43a5265310a444525de1a031992961da08a94
analiavargas <vargas_va@live.com.ar>
2021-04-12 21:52:14 -0300

    Se generó el nuevo endpoint: ultimoProducto

--- src/routes/api/apiProductos.js
+++ src/routes/api/apiProductos.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const apiProductosController = require('../../controllers/api/apiProductosController');
 
 router.get('/', apiProductosController.listado);
+router.get('/ultimo', apiProductosController.ultimoProducto)
 router.get('/:id', apiProductosController.detalle);
 
 module.exports = router;
